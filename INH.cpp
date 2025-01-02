9ac5bc26417392b492ddeaa26dd17233a7f8e096
Noel-MPAWA <noelmpawa002@gmail.com>
2025-01-02 07:49:56 +0300

    Simple code 2

---
+++ INH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+class Animal {
+public:
+    void eat() { std::cout << "Eating...\n"; }
+};
+class Dog : public Animal {
+public:
+    void bark() { std::cout << "Barking...\n"; }
+};
+int main() {
+    Dog dog;
+    dog.eat();
+    dog.bark();
+    return 0;
+}
