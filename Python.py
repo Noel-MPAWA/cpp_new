638676e4451a168b9abf0f925a4f75ea85088009
Noel-MPAWA <noelmpawa002@gmail.com>
2025-04-17 19:39:04 +0300

    My first python code

---
+++ Python.py
@@ -0,0 +1,7 @@
+# Add two numbers entered by a user 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+
+print("The sum is:", sum)
