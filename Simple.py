b95f2fcb859f85a9a9dc882e14537aa5c1aa0005
Noel-MPAWA <noelmpawa002@gmail.com>
2025-04-18 09:51:04 +0300

    Adding age question in my code

--- Simple.py
+++ Simple.py
@@ -4,4 +4,17 @@ Gender = input("Enter your gender in small 'm' or 'f': ")
 if Gender == 'm':
     print("Hello gentleman")
 else:
-    print("Hello lady")
+    print("Hello lady ")  
+
+Age = int(input("Enter your Age: "))
+
+if Age < 18:
+    print("You are a minor.")
+elif Age >= 18 and Age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+
+
+
